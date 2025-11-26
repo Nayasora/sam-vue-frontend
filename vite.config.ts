9ac2d886ceadde46520e8f9fd877663232cf137c
nayasoru <Sayanorazgaly@gmail.com>
2025-11-27 03:08:11 +0500

    Добавлен allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
-    allowedHosts: ['localhost', '.localhost'],
+    allowedHosts: ['localhost', '.localhost', 'sam-market.kz'],
     watch: {
       usePolling: true,
     },
